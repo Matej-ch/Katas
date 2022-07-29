e3f4213610ec68b3b0f9760b70566e5182999634
Matej <matejchalachan@gmail.com>
2022-07-29 19:58:33 +0200

    joshephus permutation works on basic set of tests

--- src/JPermutation.php
+++ src/JPermutation.php
@@ -13,50 +13,24 @@ class JPermutation
 
     public static function joshephus(array $n, int $k): array
     {
-        $k--;
-
-        $index = 0;
-
-        if($k === 1) {
-            return $n;
+        if(empty($n)) {
+            return [];
         }
 
-        self::josh($n, $k, $index, $result);
+        --$k;
+        $index = $k;
 
+        $result = [];
+
+        while (count($n) > 1) {
+            $result[] = $n[$index];
+            unset($n[$index]);
+            $n = array_values($n);
+
+            $index = ($index + $k) % count($n);
+        }
+
+        $result[] = $n[0];
         return $result;
-        echo '<pre>' . print_r($result, true) . '</pre>';
-        die();
-    }
-
-    public static function josh(array $n, int $k, int $index, &$result): void
-    {
-        if (count($n) === 1) {
-            $result[] = $n[0];
-            return;
-        }
-
-        $index = (($index + $k) % count($n));
-
-        /*if($index <= count($n)) {
-            return;
-        }*/
-
-        echo '<pre>'.print_r($result,true).'</pre>';
-        echo '<pre>'.print_r($index,true). "\n";
-        $result[] = $n[$index];
-        unset($n[$index]);
-
-        self::josh($n, $k, $index, $result);
-        //if($index > -1) {
-
-        //}
-
-
-        /*echo '<pre>$result: '.print_r($result,true)."\n";
-        echo '<pre>$index: '.print_r($index,true)."\n";
-        echo '<pre>$k: '.print_r($k,true)."\n";
-        echo '<pre>$n: '.print_r(array_values($n),true)."\n";die();*/
-
-
     }
 }
